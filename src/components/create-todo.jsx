dfe806edf79e87fa21035d8fe0d398c6150dc451
kavikarthik-interloid <kavikarthik@interloid.com>
2026-07-09 14:47:16 +0530

    dialog-component-fixed

--- src/components/create-todo.jsx
+++ src/components/create-todo.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { createTodo } from "../api/todo";
-import { IoIosClose } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 function TodoForm({ onTodoCreated, showNotification, onClose }) {
   const [formData, setFormData] = useState({
@@ -38,130 +38,130 @@ function TodoForm({ onTodoCreated, showNotification, onClose }) {
 
   return (
     <>
-    <div className="fixed inset-0 bg-black/40 backdrop-blur-sm transition-opacity duration-300"/>
-    <div className="fixed top-50 max-w-4xl mx-auto shadow-md w-3/4 h-112 p-5! inset-0 flex flex-col items-center backdrop-blur-md justify-start bg-white rounded-xl z-1 ">
-      <h2 className="w-full py-2 px-3 text-blue-500 text-center font-bold text-xl underline decoration-blue-400 underline-offset-8 font-inter">
-        {" "}
-        Create TO-DO{" "}
-      </h2>
-      <form
-        onSubmit={handleSubmit}
-        className="w-full grid grid-cols-2 p-5 gap-y-4 gap-x-5 font-inter"
-      >
-        <div className="flex flex-col gap-1">
-          <label htmlFor="title" className="font-medium">
-            Title
-          </label>
-          <input
-            id="title"
-            type="text"
-            name="title"
-            value={formData.title}
-            onChange={handleChange}
-            placeholder="Enter Title"
-            className="p-2 border border-gray-300 rounded-md text-sm"
-          />
-        </div>
-        <div className="flex flex-col gap-1">
-          <label htmlFor="description" className="font-medium">
-            Description
-          </label>
-          <textarea
-            id="description"
-            type="text"
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            placeholder="Enter description"
-            className="mobile:p-1 p-2 border border-gray-300 rounded-md text-sm"
-          />
-        </div>
-        <div className="flex flex-col gap-1">
-          <label htmlFor="priority" className="font-medium">
-            Priority
-          </label>
+      <div className="fixed  inset-0 bg-black/40 backdrop-blur-sm transition-opacity duration-300" z-0 />
+      <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[95%] max-w-4xl bg-white rounded-xl shadow-xl p-6 z-1 ">
+        <h2 className="w-full py-2 px-3 relative text-blue-500 text-center font-bold text-xl underline decoration-blue-400 underline-offset-8 font-inter">
+          {" "}
+          Create TO-DO{" "}
+        </h2>
+        <form
+          onSubmit={handleSubmit}
+          className="w-full grid md:grid-cols-2 gap-y-4 gap-x-5 font-inter"
+        >
+          <div className="flex flex-col gap-1">
+            <label htmlFor="title" className="font-medium">
+              Title
+            </label>
+            <input
+              id="title"
+              type="text"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+              placeholder="Enter Title"
+              className="p-2 border border-gray-300 rounded-md text-sm"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="description" className="font-medium">
+              Description
+            </label>
+            <textarea
+              id="description"
+              type="text"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              placeholder="Enter description"
+              className="mobile:p-1 p-2 border border-gray-300 rounded-md text-sm"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="priority" className="font-medium">
+              Priority
+            </label>
 
-          <select
-            id="priority"
-            name="priority"
-            value={formData.priority}
-            onChange={handleChange}
-            className="p-2  border border-gray-300 rounded-md text-sm"
-          >
-            <option value="" disabled>
-              Select Option
-            </option>
-            <option value="LOW">Low</option>
-            <option value="MEDIUM">Medium</option>
-            <option value="HIGH">High</option>
-          </select>
-        </div>
-        <div className="flex flex-col gap-1">
-          <label htmlFor="due-date" className="font-medium">
-            Due-Date
-          </label>
-          <input
-            id="due-date"
-            type="date"
-            name="due_date"
-            value={formData.due_date}
-            onChange={handleChange}
-            placeholder="Enter date"
-            className="p-2  border border-gray-300 rounded-md text-sm"
-          />
-        </div>
-        <div className="flex flex-col gap-1">
-          <label htmlFor="category" className="font-medium">
-            Category
-          </label>
-          <input
-            id="category"
-            type="text"
-            name="category"
-            value={formData.category}
-            onChange={handleChange}
-            placeholder="Enter category"
-            className="p-2  border border-gray-300 rounded-md text-sm"
-          />
-        </div>
-        <div className="flex flex-col gap-1">
-          <label htmlFor="tags" className="font-medium">
-            Tags
-          </label>
-          <input
-            id="tags"
-            type="text"
-            name="tags"
-            value={formData.tags}
-            onChange={handleChange}
-            placeholder="Enter tags"
-            className="p-2  border border-gray-300 rounded-md text-sm"
-          />
-        </div>
-        <div className="col-span-full flex justify-center gap-4 mt-4">
-          <button
-            type="submit"
-            className="w-30 py-2 radius px-1 bg-blue-600 shadow-md text-white hover:bg-blue-700"
-          >
-            Create
-          </button>
-          <button
-            type="button"
-            onClick={onClose}
-            className="w-30 p-2 radius bg-blue-600 text-white shadow-md hover:bg-blue-700"
-          >
-            Cancel
-          </button>
-          <button
-            type="button"
-            onClick={onClose}
-            className="w-fit top-[-10px] right-[-10px] absolute p-2 rounded-full bg-blue-600 text-white shadow-md hover:bg-blue-700 transition-colors"
-          >
-            <IoIosClose />
-          </button>
-        </div>
-      </form>
-    </div>
+            <select
+              id="priority"
+              name="priority"
+              value={formData.priority}
+              onChange={handleChange}
+              className="p-2  border border-gray-300 rounded-md text-sm"
+            >
+              <option value="" disabled>
+                Select Option
+              </option>
+              <option value="LOW">Low</option>
+              <option value="MEDIUM">Medium</option>
+              <option value="HIGH">High</option>
+            </select>
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="due-date" className="font-medium">
+              Due-Date
+            </label>
+            <input
+              id="due-date"
+              type="date"
+              name="due_date"
+              value={formData.due_date}
+              onChange={handleChange}
+              placeholder="Enter date"
+              className="p-2  border border-gray-300 rounded-md text-sm"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="category" className="font-medium">
+              Category
+            </label>
+            <input
+              id="category"
+              type="text"
+              name="category"
+              value={formData.category}
+              onChange={handleChange}
+              placeholder="Enter category"
+              className="p-2  border border-gray-300 rounded-md text-sm"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="tags" className="font-medium">
+              Tags
+            </label>
+            <input
+              id="tags"
+              type="text"
+              name="tags"
+              value={formData.tags}
+              onChange={handleChange}
+              placeholder="Enter tags"
+              className="p-2  border border-gray-300 rounded-md text-sm"
+            />
+          </div>
+          <div className="col-span-full flex  justify-end gap-4 mt-4 font-bold">
+            <button
+              type="button"
+              onClick={onClose}
+              className="w-30 p-2 radius bg-gray-50 text-blue-600 shadow-sm hover:bg-gray-100"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="w-30 py-2 radius px-1 bg-blue-600 shadow-md text-white hover:bg-blue-700"
+            >
+              Create
+            </button>
+            <button
+              type="button"
+              onClick={onClose}
+              className="w-fit top-0 rounded-sm right-0 absolute p-2  text-blue-600 hover:text-blue-700  transition-colors"
+            >
+           <IoClose />
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
--- src/components/delete-todo.jsx
+++ src/components/delete-todo.jsx
@@ -1,5 +1,5 @@
 import { deleteTodo } from "../api/todo";
-import { IoIosClose } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 const DeleteForm = ({ todo, onClose, onDeleted, showNotification }) => {
   const handleDelete = async (id) => {
@@ -14,37 +14,37 @@ const DeleteForm = ({ todo, onClose, onDeleted, showNotification }) => {
   };
   return (
     <>
-   <div className="fixed inset-0 bg-black/40 backdrop-blur-sm transition-opacity duration-300"/>
-    <div className="fixed top-50 max-w-xl mx-auto font-inter shadow-md w-1/2 h-75 p-5! gap-10 inset-0 flex flex-col items-center backdrop-blur-md justify-center bg-white rounded-lg z-1 ">
-      <h2 className="w-full py-2 px-3 text-blue-500 font-bold text-center text-xl underline decoration-blue-400 underline-offset-8 font-inter">
-        {" "}
-        Delete TO-DO{" "}
-      </h2>
-      <p className="text-md">Are you sure you want to delete this todo?</p>
-      <div className="flex gap-4">
-        <button
-          onClick={handleDelete}
-          className="w-30 p-2 radius bg-red-600 text-white shadow-md hover:bg-blue-700 font-bold"
-        >
+      <div className="fixed inset-0 bg-black/40 backdrop-blur-sm transition-opacity duration-300" />
+      <div className="fixed top-75 max-w-xl mx-auto font-inter shadow-md w-1/2 h-fit p-5! gap-10 inset-0 flex flex-col items-center backdrop-blur-md justify-center bg-white rounded-lg z-1 ">
+        <h2 className="w-full relative py-2 px-3 text-blue-500 font-bold text-center text-xl underline decoration-blue-400 underline-offset-8 font-inter">
           {" "}
-          confirm{" "}
-        </button>
-        <button
-          onClick={onClose}
-          className="w-30 p-2 radius bg-blue-600 text-white shadow-md hover:bg-blue-700 font-bold"
-        >
-          {" "}
-          cancel
-        </button>
-        <button
-          type="button"
-          onClick={onClose}
-          className="w-fit top-[-10px] right-[-10px] absolute p-2 rounded-full bg-blue-600 text-white shadow-md hover:bg-blue-700 transition-colors"
-        >
-          <IoIosClose />
-        </button>
+          Delete TO-DO{" "}
+        </h2>
+        <p className="text-md">Are you sure you want to delete this todo?</p>
+        <div className="flex gap-4">
+          <button
+            onClick={onClose}
+            className="w-30 p-2 radius bg-gray-0 text-rec-600 shadow-sm hover:bg-gray-100 font-bold"
+          >
+            {" "}
+            cancel
+          </button>
+          <button
+            onClick={handleDelete}
+            className="w-30 p-2 radius bg-red-600 text-white shadow-md hover:bg-red-700 font-bold"
+          >
+            {" "}
+            confirm{" "}
+          </button>
+            <button
+              type="button"
+              onClick={onClose}
+              className="w-fit top-0 rounded-sm right-0 absolute p-2  text-blue-600 hover:text-blue-700  transition-colors"
+            >
+           <IoClose />
+            </button>
+        </div>
       </div>
-    </div>
     </>
   );
 };
