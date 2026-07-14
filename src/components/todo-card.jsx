2ada28d3d0e4a59f1df79eb897242df457007caa
kavikarthik-interloid <kavikarthik@interloid.com>
2026-07-14 10:14:13 +0530

    GET Method completed

--- src/components/todo-card.jsx
+++ src/components/todo-card.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { getTodo } from "../api/todo";
-import CreateTodo from "../components/create-todo";
+// import CreateTodo from "../components/create-todo";
 
 const TodoCard = () => {
   const [todoData, setTodoData] = useState([]);
@@ -11,23 +11,21 @@ const TodoCard = () => {
   useEffect(() => {
     fetchtodos();
   }, []);
+  // console.log(todoData)
   return (
     <div>
       {todoData.map((item) => (
-        <>
         <div key={item.id}>
           <span>{item.title}</span>
           <span>{item.category}</span>
           <span>{item.description}</span>
           <span>{item.priority}</span>  
           <span>{item.due_date}</span>
-          {item.tags.map((tag) => (
-            <span key={tag}>{tag}</span>
+          {item.tags.map((index, tag ) => (
+            <span key={index}>{tag}</span>
           ))}
           <span>{item.complete}</span>
         </div>  
-          {/* <button> Delete </button> */}
-           </>
       ))}
     </div>
   );
